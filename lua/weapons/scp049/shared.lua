c288f0e3ed9a79a70713657573477b8290b78792
Angel. <revanangelpro@gmail.com>
2025-01-02 22:16:25 +0100

    Maybe not, maybe yes

--- lua/weapons/scp049/shared.lua
+++ lua/weapons/scp049/shared.lua
@@ -3,7 +3,9 @@ if not scp049 then scp049 = {} end
 local revscp049 = guthscp.modules.revscp049
 local config049 = guthscp.configs.revscp049
 
-AddCSLuaFile("autorun/revscp049/shared.lua")
+if SERVER then
+    AddCSLuaFile("autorun/revscp049/shared.lua")
+end
 include("autorun/revscp049/shared.lua")
 
 if not guthscp then
